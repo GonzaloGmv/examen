75d7b42e44021868569284fa14df5dc86a613498
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-14 11:39:19 +0100

    pedir palabra

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,9 @@
-palabra = input("Escriba una palabra: ")
+vocales ='aeiou'
+while True:
+    palabra = input("Escriba una palabra: ")
+    letras = palabra.split()
+    if len(letras) == 1:
+        break
+    else:
+        print("Eso no es solo una palabra")
+        pass
