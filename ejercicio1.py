a748d5b6b85af38a56215924bd5aa15bdd9f1a2e
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-14 11:59:30 +0100

    creo que esta ya

--- ejercicio1.py
+++ ejercicio1.py
@@ -7,3 +7,29 @@ while True:
     else:
         print("Eso no es solo una palabra")
         pass
+
+posicion = -1
+puntos_kevin = 0
+puntos_stuart = 0
+for letras in palabra:
+    if vocales.__contains__(letras):
+        if palabra.count(letras) > 1:
+            posicion = palabra.index(letras, posicion + 1)
+            puntos_kevin = puntos_kevin + len(palabra) - palabra.index(letras, posicion)
+        else:
+            puntos_kevin = puntos_kevin + len(palabra) - palabra.index(letras)
+    else:
+        if palabra.count(letras) > 1:
+            posicion = palabra.index(letras, posicion + 1)
+            puntos_stuart = puntos_stuart + len(palabra) - palabra.index(letras, posicion)
+        else:
+            puntos_stuart = puntos_stuart + len(palabra) - palabra.index(letras)
+
+print("Kevin: ", puntos_kevin)
+print("Stuart: ", puntos_stuart)
+if puntos_stuart > puntos_kevin:
+    print("El ganador es Stuart, ha obtenido ", puntos_stuart, " puntos")
+elif puntos_kevin > puntos_stuart:
+    print("El ganador es Kevin, ha obtenido ", puntos_kevin, " puntos")
+else:
+    print("Draw")
